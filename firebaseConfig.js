4d43a260aa53f53c2f15c643af42810cb2a37eea
pasion-285567 <bro.emond.8@gmail.com>
2025-10-20 12:19:49 +0800

    maraming binago

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+//Para ito sa backend ng system kaya ginawa ko siyang hiwalay na file para mas madali i-manage
+// At para hindi na inuulit-ulit yung firebase config sa bawat js file
+// Just import this file sa mga js files na kailangan yung firebase config
+// Example: import { firebaseConfig } from './firebaseConfig.js';
+// Then gamitin yung firebaseConfig variable sa initializeApp(firebaseConfig);
+// Hahahahahahaahaha nagcomment pa talaga, sipag ah
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDb0tPX65XoA-VWzUZMq0eWlxWBPrvawRI",
+    authDomain: "jabobsresto.firebaseapp.com",
+    projectId: "jabobsresto",
+    storageBucket: "jabobsresto.firebasestorage.app",
+    messagingSenderId: "792607578472",
+    appId: "1:792607578472:web:de2b49effd069eeb5a1edd",
+    measurementId: "G-4JRPS8C2YR"
+};
